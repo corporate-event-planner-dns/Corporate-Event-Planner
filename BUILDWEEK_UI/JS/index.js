e50caec21e04506fc45e9bcf6f176c9f98015f0a
Sable Childs <sablechilds@Sables-MacBook-Air.local>
2019-06-24 00:12:39 -0400

    added-event-listenerener-and-matching-nav-page-mostly-complete

---
+++ BUILDWEEK_UI/JS/index.js
@@ -0,0 +1,27 @@
+
+var slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+  var i;
+  const slides = document.querySelectorAll(".mySlides")
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 4000); 
+};
+
+
+
+const toggleMenuu = () => {
+  menu1.classList.toggle("show");
+}
+const menu1 = document.querySelector(".menu1")
+
+const menuBtn1 = document.querySelector(".menuBtn1")
+
+menuBtn1.addEventListener("click", toggleMenuu)
