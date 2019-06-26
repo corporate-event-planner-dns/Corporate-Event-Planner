7312b23a7e9efdd17067982b2cad6a92a4b84563
Denvinn Magsino <denvinnmagsino@Denvinns-MacBook-Air.local>
2019-06-26 00:57:50 -0700

    added 2 more pages

--- BUILDWEEK_UI/LESS/about.js
+++ BUILDWEEK_UI/LESS/about.js
@@ -2,7 +2,7 @@ const toggleMenu = () =>{
     menu.classList.toggle("show");
 }
 
-const menu=document.querySelector(".menu");
+const menu=document.querySelector(".leftNavBar");
 
 const menuBtn=document.querySelector(".menuBtn");
 
